415b1376f1703ce0766b353771a5eb4b1f0d0fbc
Patrick Barbosa <patrick_souza98@hotmail.com>
2021-09-07 17:08:30 -0300

    melhorada logica do commit anterior

--- resources/views/agendamento/agendPag2Mesa.blade.php
+++ resources/views/agendamento/agendPag2Mesa.blade.php
@@ -27,7 +27,7 @@
                             <div class="box2"><p>Area de lazer></p></div>
                             <div class="boxMesa">
                                 @for($i = 1; $i <= 30; $i++)
-                                    @if(count($reservas->where('dia', $data)->where('id_mesa', $i)) == 5)
+                                    @if(count($reservas->where('dia', $data)->where('id_mesa', $i)->where('local', $local)) == 5)
                                 <label class="containerMesa">
                                     <input type="radio" name="id_mesa" class="radioButton" value="{{$i}}" disabled>
                                     <span class="checkmark">{{$i}}</span>
