0e87e6a912c2fa84ee2dce7046cc310da9b46b21
Patrick Barbosa <patrick_souza98@hotmail.com>
2021-09-07 12:15:31 -0300

    ajustada model e migration da tabela reservas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call(ConsultorSeeder::class);
-        //$this->call(MesaSeeder::class);
-        //$this->call(CadeiraSeeder::class);
+        $this->call(MesaSeeder::class);
+        $this->call(CadeiraSeeder::class);
     }
 }
