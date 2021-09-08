cf967256c5265c3cf7c1d96e14e76a896e82f125
Patrick Barbosa <patrick_souza98@hotmail.com>
2021-09-08 01:22:54 -0300

    Passado todo o processo da reserva pra 1 controller só, APRENDI XD

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::get('login', 'App\Http\Controllers\LogarController@index');
 Route::post('login', 'App\Http\Controllers\LogarController@logar')->name('login');
 Route::get('reserva', 'App\Http\Controllers\ReservaController@index')->name('reserva')->middleware('auth');
 Route::post('reserva', 'App\Http\Controllers\ReservaController@store')->name('reserva')->middleware('auth');
-Route::get('reserva2', 'App\Http\Controllers\Reserva2Controller@index')->name('reserva2')->middleware('auth');
-Route::post('reserva2', 'App\Http\Controllers\Reserva2Controller@store')->name('reserva2')->middleware('auth');
-Route::get('reserva3', 'App\Http\Controllers\Reserva3Controller@index')->name('reserva3')->middleware('auth');
-Route::post('reserva3', 'App\Http\Controllers\Reserva3Controller@store')->name('reserva3')->middleware('auth');
-Route::get('reserva4', 'App\Http\Controllers\Reserva4Controller@index')->name('reserva4')->middleware('auth');
-Route::post('reserva4', 'App\Http\Controllers\Reserva4Controller@store')->name('reserva4')->middleware('auth');
+Route::get('reserva2', 'App\Http\Controllers\ReservaController@mostrarMesas')->name('reserva2')->middleware('auth');
+Route::post('reserva2', 'App\Http\Controllers\ReservaController@escolherMesa')->name('reserva2')->middleware('auth');
+Route::get('reserva3', 'App\Http\Controllers\ReservaController@mostrarCadeiras')->name('reserva3')->middleware('auth');
+Route::post('reserva3', 'App\Http\Controllers\ReservaController@escolherCadeira')->name('reserva3')->middleware('auth');
+Route::get('reserva4', 'App\Http\Controllers\ReservaController@revisarReserva')->name('reserva4')->middleware('auth');
+Route::post('reserva4', 'App\Http\Controllers\ReservaController@confirmarReserva')->name('reserva4')->middleware('auth');
 
 Route::get('/visualizando-email', function (){
     return new \App\Mail\Confirmacao('Patrick', '2021-09-08', 'São Paulo', '3', '14');
