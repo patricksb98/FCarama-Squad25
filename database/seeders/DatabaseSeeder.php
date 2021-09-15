bb09e141be65e0d7c926299930eabaa8c0c2b377
Patrick Barbosa <patrick_souza98@hotmail.com>
2021-09-15 01:57:48 -0300

    att seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call(ConsultorSeeder::class);
-        //$this->call(MesaSeeder::class);
-        //$this->call(CadeiraSeeder::class);
+        //$this->call(ConsultorSeeder::class);
+        $this->call(MesaSeeder::class);
+        $this->call(CadeiraSeeder::class);
     }
 }
