e66c9458f5cd7e64e818f4b124471af101ee2cc3
BrunoCosta-P <brunocosta524@hotmail.com>
2021-09-16 15:24:45 -0300

    ajuste login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/responsivo/responsivoLogin.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Encontro Dos Laranjas</title>
 </head>
 <body>
